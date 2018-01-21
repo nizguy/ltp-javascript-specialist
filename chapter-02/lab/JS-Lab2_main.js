1f683fdea8f0642f6ca0f1702db6ec8ac06ce783
Mark Burns <mark.007.99@gmail.com>
2018-01-21 08:01:16 -0500

    Javascript Chapter 2 Complete
    Complete all exercises and lab plus extra lab for Javascript chapter 2

---
+++ chapter-02/lab/JS-Lab2_main.js
@@ -0,0 +1,23 @@
+window.onload = function()
+	{
+		document.getElementById('addingButton').addEventListener('click', addNumbers);
+	document.getElementById('multiplyButton').addEventListener('click', multiplyNumbers);
+	}
+
+	function addNumbers(e)
+	{
+		var answer = "";
+		
+		answer += + document.getElementById('inputOne').value + + document.getElementById('inputTwo').value 
+		
+		alert(answer)
+	}
+
+	function multiplyNumbers(e)
+	{
+		var answerMultiply = "";
+		
+		answerMultiply += + document.getElementById('inputOneOne').value * document.getElementById('inputTwoTwo').value 
+		
+		alert(answerMultiply)
+	}	
